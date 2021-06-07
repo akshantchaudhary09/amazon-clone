99aaf7ccd35bdfbeae96257fab2339e9fad3f432
akshantchaudhary09 <chaudharyakshant2001@gmail.com>
2021-06-07 12:27:19 +0530

    setting up react context api

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,17 +3,38 @@ import ReactDOM from 'react-dom';
 import './Home.css';
 import Product from '../Product/Product.js';
 import productImg from './images/background-img.jpg';
+import Grid from '@material-ui/core/Grid';
+import productList from "../Product/productList.js"
 
 const Home = () => {
   return (
     <div className="home">
       {/* <img className="background-img" src={backgroundImg} alt="background-img"></img> */}
-      <Product 
-        heading="Iphone X"
-        price="$500"
-        rating="⭐⭐⭐"
-        img={productImg}
-      />
+      <Grid container spacing={3} className="home-container1">
+      {productList[0].map((product) => 
+          <Grid item xs={6}>
+            <Product 
+              heading={product.heading}
+              price={product.price}
+              rating={product.rating}
+              img={product.img}
+            />
+          </Grid>
+        )}
+        {productList[1].map((product) => 
+          <Grid item xs={4}>
+            <Product 
+              heading={product.heading}
+              price={product.price}
+              rating={product.rating}
+              img={product.img}
+            />
+          </Grid>
+        )}    
+        
+      </Grid>
+      
+      
     </div>
   );
   
---
+++ src/components/Product/productList.js
@@ -0,0 +1,49 @@
+import iphonexImg from './images/iphone_x.jpg';
+import silentPatientImg from './images/silent_patient.jpg';
+import ipadImg from './images/ipad.png';
+import macbookImg from './images/macbook-air-gold.jpg';
+import jeansImg from './images/jeans.jpg';
+
+const productList = [
+    [
+        {
+            key:1,
+            heading:"Iphone X 64GB white variant",
+            price:"$500",
+            rating:"⭐⭐⭐⭐",
+            img:iphonexImg
+        },
+        {
+            key:2,
+            heading:"The silent patient new york bestseller",
+            price:"$50",
+            rating:"⭐⭐⭐⭐⭐",
+            img:silentPatientImg
+        }
+    ],
+    [
+        {
+            key:1,
+            heading:"Apple ipad 128GB",
+            price:"$300",
+            rating:"⭐⭐⭐",
+            img:ipadImg
+        },
+        {
+            key:2,
+            heading:"Apple macbook gold 256GB SSD",
+            price:"$1000",
+            rating:"⭐⭐⭐⭐⭐",
+            img:macbookImg
+        },
+        {
+            key:3,
+            heading:"Levis jeans blue denim",
+            price:"$100",
+            rating:"⭐⭐⭐⭐",
+            img:jeansImg
+        }
+    ]
+]
+
+export default productList;
