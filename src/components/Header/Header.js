1505c674aae13808240c31194909f64d6221e7c8
akshantchaudhary09 <chaudharyakshant2001@gmail.com>
2021-06-07 22:16:55 +0530

    adding the basket functionality

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Link } from "react-router-dom";
 import './Header.css';
 import amazonLogo from './images/Amazon_logo.svg.png';
 import SearchIcon from '@material-ui/icons/Search';
 import Button from '@material-ui/core/Button';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { useStateValue } from '../../StateProvider.js';
 
 const Header = () => {
+
+  const [{ basket, user }, dispatch] = useStateValue();
+
   return (
     <div className="header">
-      <img className="amazon-logo" src={amazonLogo} alt="amazon-logo"></img>
+      <Link to="/">
+        <img className="amazon-logo" src={amazonLogo} alt="amazon-logo"></img>
+      </Link>
       <input type="text" placeholder="Search.."></input>
       <button className="search-bar-btn"><SearchIcon fontSize="medium"/></button>
       <Button className="btn">Login</Button>
       <Button className="btn"><span>Returns</span><br/> <span>& Orders</span></Button>
       <Button className="btn"><span>Your</span><br /> <span>prime</span></Button>
-      <Button className="btn"><ShoppingCartIcon /></Button>
+      <Link to="/checkout">
+        <Button className="btn"><ShoppingCartIcon /></Button>
+        <span className="count-basket">{basket?.length}</span>
+      </Link>
     </div>
   );
 }
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import './Product.css';
+import { useStateValue } from '../../StateProvider.js';
 
 const Product = (props) => {
+
+  const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        heading: props.heading,
+        img: props.img,
+        price: props.price,
+        rating: props.rating,
+      },
+    });
+  };
+
+
   return (
     <div className="product">
       <p>{props.heading}</p>
@@ -9,7 +26,7 @@ const Product = (props) => {
       <p>{props.rating}</p>
       <br></br>
       <img src={props.img} alt="product-img"></img>
-      <button>Add to cart</button> 
+      <button onClick={addToBasket}>Add to cart</button> 
     </div>
   );
 
